b36f2e3145e9233349528151eda7a5ee32ebd720
omaritooo <themedic227@gmail.com>
2024-02-19 22:39:22 +0200

    Fix go home button

--- src/layouts/ErrorLayout.tsx
+++ src/layouts/ErrorLayout.tsx
@@ -12,9 +12,9 @@ export const ErrorLayout = () => (
       <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
         <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0" />
 
-        <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-          <Link to="/">Go Home</Link>
-        </span>
+        <a className="relative block px-8 py-3 bg-[#1A2238] border border-current" href="/">
+          Go Home
+        </a>
       </a>
     </button>
   </main>
